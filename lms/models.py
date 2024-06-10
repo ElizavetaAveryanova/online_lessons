9fbb1e94f4da10066a697de88260179fdf4b4332
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-06-10 07:57:07 +0500

    homework_24.1_

--- lms/models.py
+++ lms/models.py
@@ -3,29 +3,35 @@ from django.db import models
 
 class Course(models.Model):
     """Курс"""
-    title = models.CharField(max_length=100, verbose_name='Название')
-    description = models.TextField(verbose_name='Описание')
-    preview = models.ImageField(upload_to='courses/', null=True, blank=True, verbose_name='Изображение')
+
+    title = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    preview = models.ImageField(
+        upload_to="lms/", null=True, blank=True, verbose_name="Изображение"
+    )
 
     def __str__(self):
-        return f'Курс {self.title}'
+        return f"Курс {self.title}"
 
     class Meta:
-        verbose_name = 'Курс'
-        verbose_name_plural = 'Курсы'
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
 
 
 class Lesson(models.Model):
     """Урок"""
-    title = models.CharField(max_length=100, verbose_name='Название')
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
-    description = models.TextField(verbose_name='Описание')
-    preview = models.ImageField(upload_to='lessons/', null=True, blank=True, verbose_name='Изображение')
-    link = models.URLField(verbose_name='Ссылка на видео-урок')
+
+    title = models.CharField(max_length=100, verbose_name="Название")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+    description = models.TextField(verbose_name="Описание")
+    preview = models.ImageField(
+        upload_to="lms/lessons", null=True, blank=True, verbose_name="Изображение"
+    )
+    link = models.URLField(verbose_name="Ссылка на видео-урок")
 
     def __str__(self):
-        return f'Урок {self.title} из курса:{self.course}'
+        return f"Урок {self.title} из курса:{self.course}"
 
     class Meta:
-        verbose_name = 'Урок'
-        verbose_name_plural = 'Уроки'
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
-from users.models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'email', 'phone', 'city')
+# Register your models here.
